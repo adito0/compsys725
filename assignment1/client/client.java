f04d07dd7718416e314eec3c226771a3c59cd8b2
Syamira <isuk218@aucklanduni.ac.nz>
2018-08-22 01:55:21 +1200

    done commenting for client, time to do readme

--- assignment1/client/client.java
+++ assignment1/client/client.java
@@ -15,7 +15,8 @@ class clientTCP {
 	private static String errorMessage;
 	private static String filename = "";
 	private static long fileSize = 0;
-	
+	private static String host = "localhost";
+	private static int port = 1500;
 	private static BufferedReader inFromUser;
 	private static Socket clientSocket;
 	private static DataOutputStream outToServer;
@@ -27,14 +28,14 @@ class clientTCP {
 	public static File file;
 
 	/** 
-	*	descripton	: 	
-	*					
+	*	descripton	: 	attemptConnection() sets up the buffers needed for communication with the server
+	*					and initiates connection through specified host and port
 	*	args		: 	NONE
 	*	returns		:	NONE
 	**/		
 	public void attemptConnection() throws Exception {
 		inFromUser = new BufferedReader(new InputStreamReader(System.in)); 
-		clientSocket = new Socket("localhost", 1500); 
+		clientSocket = new Socket(host, port); 
 		outToServer = new DataOutputStream(clientSocket.getOutputStream()); 
 		inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		fileOutToServer = new DataOutputStream(clientSocket.getOutputStream());
@@ -51,42 +52,40 @@ class clientTCP {
 	}
 	
 	/** 
-	*	descripton	: 	
-	*					
+	*	descripton	: 	readServerResponse() reads and concatenates the errorMessage sent by the server character 
+	*					by character until a <null> character is found.
 	*	args		: 	NONE
-	*	returns		:	NONE
+	*	returns		:	line - the errorMessage sent by the server without the null character
 	**/	
 	public String readServerResponse() throws Exception {
 		String line = "";
 		int character = 0;
-
+		//loop through char by char until a terminating character is found
 		while (true) {
 			try {
 				character = inFromServer.read();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-
 			if (character == 0) {
 				break;
 			}
-
+			//convert and concatenate the characters into a string
 			line = line.concat(Character.toString((char)character));
 		}
 		return line;
 	}
 
 	/** 
-	*	descripton	: 	
-	*					
+	*	descripton	: 	checkValidCommand() reads the command typed in by the user and checks if its of valid
+	*					command format and sends them to the server. Preprocessing of files are also done
+	8					for the SEND and STOR commands to prepare the client for file sending/retrieving
 	*	args		: 	NONE
 	*	returns		:	NONE
 	**/		
 	public void checkValidCommand() throws Exception {
-
 		System.out.println("enter command: ");
 		command = inFromUser.readLine();
-		
 		if (command != null) {
 			try {
 				cmd = command.substring(0,4);
@@ -131,8 +130,8 @@ class clientTCP {
 	}			
 
 	/** 
-	*	descripton	: 	
-	*					
+	*	descripton	: 	processServerResponse() reads the errorMessage sent by the server and processes
+	*					the file size sent over by the server in return for the RETR command sent
 	*	args		: 	NONE
 	*	returns		:	NONE
 	**/		
@@ -145,18 +144,19 @@ class clientTCP {
 			processServerResponse();
 		}
 		else if ((errorMessage.charAt(0) == '!') || (errorMessage.charAt(0) == '+') || (errorMessage.charAt(0) == '-')) {
-							
+			//do nothing
 		}
 		else {
+			//if server sends over the file size in return for the RETR command sent, it is saved to fileSize
 			fileSize = Long.parseLong(errorMessage.replaceAll("\\s",""));	
 		}
 	}
 
 	/** 
-	*	descripton	: 	
-	*					
+	*	descripton	: 	sendFile() sends file from the current directory to the server byte by byte when the STOR
+	*					command is sent to the server
 	*	args		: 	NONE
-	*	returns		:	NONE
+	*	returns		:	boolean true if the file was sent succesfully
 	**/		
 	public boolean sendFile(File file) throws Exception {
 		System.out.println("sendFile() called");
@@ -165,18 +165,14 @@ class clientTCP {
 		try {
 			FileInputStream fis = new FileInputStream(file);
 			BufferedInputStream bufferedInStream = new BufferedInputStream(new FileInputStream(file));
-
 			int content = 0;
-			
-			// Read and send file until the whole file has been sent
+			//read the file and write to buffer byte by byte
 			while ((content = bufferedInStream.read(bytes)) >= 0) {
 				fileOutToServer.write(bytes, 0, content);
 			}
-			
 			bufferedInStream.close();
 			fis.close();
 			fileOutToServer.flush();
-	
 		} catch (FileNotFoundException e) {
 			return false;
 		} catch (IOException e) {
@@ -186,8 +182,8 @@ class clientTCP {
 	}		
 
 	/** 
-	*	descripton	: 	
-	*					
+	*	descripton	: 	receiveFile() receives file from the server byte by byte and saves it to the currentDirectory
+	*					when the RETR command is sent
 	*	args		: 	NONE
 	*	returns		:	NONE
 	**/		
@@ -195,17 +191,15 @@ class clientTCP {
 		System.out.println("receiveFile() called");
 		FileOutputStream fileOutStream = new FileOutputStream(file, overwrite);
 		BufferedOutputStream bufferedOutStream = new BufferedOutputStream(fileOutStream);
-
-		// Read and write for all bytes
+		//read the file and write to buffer byte by byte
 		for (int i = 0; i < fileSize; i++) {
 			bufferedOutStream.write(fileInFromClient.read());
 		}
-
 		bufferedOutStream.close();
 		fileOutStream.close();
-		System.out.println(filename + " has been saved");
+		System.out.println(filename + " has been saved to the directory");
 	}
-
+	
     public static void main(String argv[]) throws Exception 
     { 
 		//create new instance of TCPServer
